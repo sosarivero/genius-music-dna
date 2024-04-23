0aee2c937d493f4f0f5f768b108b4f39cec8172b
sosarivero <81424281+sosarivero@users.noreply.github.com>
2024-04-23 16:16:41 +0100

    Add styling to search results

--- frontend/src/components/SearchResultCard.jsx
+++ frontend/src/components/SearchResultCard.jsx
@@ -1,9 +1,17 @@
+import './SearchResultCard.css';
+
 function SearchResultCard({ obj }) {
   console.log(obj);
   return (
-    <div>
+    <div className="search-result">
       <img src={obj.result.song_art_image_thumbnail_url} alt="" />
-      <b>{obj.result.full_title}</b>
+      <ul>
+        <li>
+          <b>{obj.result.title}</b>
+        </li>
+        <li>{obj.result.artist_names}</li>
+        <li>{obj.result.release_date_for_display}</li>
+      </ul>
     </div>
   );
 }
