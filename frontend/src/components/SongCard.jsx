29f0e60f709e8de2e0e3c2fb317638492a8022f6
sosarivero <81424281+sosarivero@users.noreply.github.com>
2024-04-23 10:34:30 +0100

    Render release date and producers list in song card

--- frontend/src/components/SongCard.jsx
+++ frontend/src/components/SongCard.jsx
@@ -1,6 +1,7 @@
 import { getSongById } from '../services/geniusService';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import './SongCard.css';
 
 function SongCard() {
   const [song, setSong] = useState(null);
@@ -20,11 +21,16 @@ function SongCard() {
   }, [songId]);
 
   return (
-    <div>
+    <div className="song-card">
       {song && (
         <>
           <h1>{song.full_title}</h1>
           <img src={song.header_image_url} alt="" />
+          Released in {song.release_date_for_display}
+          <b>Producers:</b>
+          {song.producer_artists.map((obj) => (
+            <li>{obj.name}</li>
+          ))}
         </>
       )}
     </div>
