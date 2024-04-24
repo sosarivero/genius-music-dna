358490ebb6b0b83efcea4a05e32d08b2a88acf65
Carlos Sosa Rivero <81424281+sosarivero@users.noreply.github.com>
2024-04-24 21:49:44 +0100

    Add MUI signup form page

--- frontend/src/router/router.jsx
+++ frontend/src/router/router.jsx
@@ -3,6 +3,7 @@ import Root from '../layouts/mainLayout';
 import Home from '../pages/Home';
 import SearchResults from '../pages/SearchResults';
 import SongCard from '../components/SongCard';
+import SignUp from '../pages/SignUp';
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
       { path: '/', element: <Home /> },
       { path: '/songs/:songId', element: <SongCard /> },
       { path: '/search', element: <SearchResults /> },
+      { path: '/signup', element: <SignUp /> },
     ],
   },
 ]);
