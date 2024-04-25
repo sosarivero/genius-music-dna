ca536ebd0795a5c0ecd17a36aaad2358b62f407e
sosarivero <81424281+sosarivero@users.noreply.github.com>
2024-04-25 14:27:56 +0100

    Comment 'allow' line in iframe

--- frontend/src/components/YoutubeEmbed.jsx
+++ frontend/src/components/YoutubeEmbed.jsx
@@ -19,7 +19,7 @@ function YoutubeEmbed({ youtubeLink }) {
         src={`https://www.youtube.com/embed/${getYTCode(youtubeLink)}`}
         title="YouTube video player"
         frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
       ></iframe>
