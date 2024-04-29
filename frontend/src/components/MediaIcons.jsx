6bbc6e8c15cce2609582b417c3f83e8edee7c228
sosarivero <81424281+sosarivero@users.noreply.github.com>
2024-04-29 16:29:48 +0100

    Make media icon links open to a new tab

--- frontend/src/components/MediaIcons.jsx
+++ frontend/src/components/MediaIcons.jsx
@@ -6,13 +6,13 @@ function MediaIcons({ media }) {
       {media.map((item, index) => {
         if (item.provider === 'soundcloud') {
           return (
-            <a href={item.url}>
+            <a href={item.url} target="_blank" rel="noopener">
               <img className="media-icon" src={'../../public/soundcloud.png'} />
             </a>
           );
         } else if (item.provider === 'spotify') {
           return (
-            <a href={item.url}>
+            <a href={item.url} target="_blank" rel="noopener">
               <img className="media-icon" src={'../../public/Spotify_Icon_RGB_White.png'} />
             </a>
           );
