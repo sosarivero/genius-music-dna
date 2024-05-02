39db5cbbaf85dc2a4369506801b83b8b9c85e76f
sosarivero <81424281+sosarivero@users.noreply.github.com>
2024-05-02 12:35:20 +0100

    Create branch for vercel deployment

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://genius-music-dna.onrender.com/api/',
   headers: {
     'Access-Control-Allow-Origin': '*',
   },
